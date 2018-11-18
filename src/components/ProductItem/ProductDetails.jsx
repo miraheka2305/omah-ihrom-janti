f3b7d150d0c53fba18c7142e997aaca806715ceb
Mirah Eka <miraheka@Proofns-MacBook-Pro.local>
2018-11-18 11:43:05 +0700

    Product Details responsive

--- src/components/ProductItem/ProductDetails.jsx
+++ src/components/ProductItem/ProductDetails.jsx
@@ -27,9 +27,11 @@ export default class ProductDetails extends Component {
             <Button src={XBtn} onClick={this.props.onClose} />
           </XButton>
           <Wrapper>
-            <ImgWrapper onMouseMove={this.handleMouseMove} style={this.state}>
-              <ItemImg src={this.props.data.image} />
-            </ImgWrapper>
+            <Img>
+              <ImgWrapper onMouseMove={this.handleMouseMove} style={this.state}>
+                <ItemImg src={this.props.data.image} />
+              </ImgWrapper>
+            </Img>
             <DescWrapper>
               <NameText>{this.props.data.name}</NameText>
               <PriceText>{this.props.data.price}</PriceText>
@@ -62,34 +64,69 @@ const Wrapper = styled.div`
 `;
 
 const ItemImg = styled.img`
-  width: 300px;
-  height: 300px;
+  width: 120px;
+  height: 120px;
   display: block;
   pointer-event: none;
+  @media only screen and (min-width: 768px) {
+    width: 200px;
+    height: 200px;
+  }
+  @media only screen and (min-width: 1024px) {
+    width: 300px;
+    height: 300px;
+  }
+`;
+
+const Img = styled.div`
+  margin: 0 auto;
+  width: fit-content;
 `;
 
 const ImgWrapper = styled.figure`
   display: inline-block;
-  margin: 50px 0 0 20px;
   cursor: pointer;
   background-repeat: no-repeat;
+  margin: 20px 0 0 0px;
   &:hover ${ItemImg} {
     opacity: 0;
+  }
+  @media only screen and (min-width: 768px) {
+    margin: 50px 0 0 20px;
   }
 `;
 
 const DescWrapper = styled.div`
-  float: right;
-  padding: 30px 20px 20px 10px;
-  width: 350px;
   height: 100%;
   text-align: left;
+  padding: 0 15px;
+  @media only screen and (min-width: 1025px) {
+    padding: 20px;
+    float: right;
+  }
+`;
+// width: 350px;
+
+const NameText = styled.h1`
+  font-size: 14px;
+  text-align: center;
+  @media only screen and (min-width: 768px) {
+    font-size: 20px;
+  }
 `;
 
-const NameText = styled.h1``;
-
-const PriceText = styled.h2``;
+const PriceText = styled.h2`
+  font-size: 12px;
+  text-align: center;
+  @media only screen and (min-width: 768px) {
+    font-size: 16px;
+  }
+`;
 
 const DescText = styled.p`
   text-align: justify;
+  font-size: 12px;
+  @media only screen and (min-width: 768px) {
+    font-size: 16px;
+  }
 `;
